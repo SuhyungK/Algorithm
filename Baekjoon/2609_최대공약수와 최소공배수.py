ee5496202d0a6746789fcd3dcb07db9c984ef97b
Suhyung <dook4966@gmail.com>
2022-07-30 07:01:53 +0900

    0730

---
+++ Baekjoon/2609_최대공약수와 최소공배수.py
@@ -0,0 +1,31 @@
+# 최소공배수 = (두 수의 곱) / (최대공약수)
+# 최대공약수를 먼저 구하고 두 수를 곱한 뒤 나눠줌
+
+
+def gcd(M, m):
+    if m == 0:
+        return M
+    else:
+        return gcd(m, M % m)
+
+a, b = map(int, input().split())
+g = gcd(a, b)
+print(g)
+print(a * b // g)
+
+
+
+# 처음 풀이
+# 굳이 최소공배수 함수를 쓰는 게 비효율적
+def gcd(M, m):
+    if M % m:
+        return gcd(m, M % m)
+    else:
+        return m 
+
+def lcm(M, m):
+    return M * m // gcd(M, m)
+
+a, b = map(int, input().split())
+print(gcd(a, b))
+print(lcm(a, b))
