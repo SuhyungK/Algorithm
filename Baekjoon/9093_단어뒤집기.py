5ed1037b945c3d4d4baf4cba6a308a9147540ead
Suhyung <dook4966@gmail.com>
2022-07-31 04:05:48 +0900

    0731

--- Baekjoon/9093_단어뒤집기.py
+++ Baekjoon/9093_단어뒤집기.py
@@ -1,3 +1,17 @@
+# 116ms
+# join메서드로 리스트에 있는 단어들 문자열로 출력
+# input자체에 readline 넣어줘서 처음부터 읽는 속도 빠르게
+import sys
+input = sys.stdin.readline
+
+for t in range(int(input())):
+    S = input().split()
+    reverse = [s[::-1] for s in S]
+    print(" ".join(reverse))
+
+
+
+
 # 176ms
 # sys.stdin.readline()이 input()보다 확실히 빠르다
 # 하나씩 입력받은 다음에 다시 출력하면서 거꾸로 출력
