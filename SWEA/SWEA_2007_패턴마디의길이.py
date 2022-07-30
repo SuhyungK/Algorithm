dfe946f7c2222947c973597b48b21bae47935f88
Suhyung <dook4966@gmail.com>
2022-07-31 03:16:48 +0900

    0731

---
+++ SWEA/SWEA_2007_패턴마디의길이.py
@@ -0,0 +1,12 @@
+T = int(input())
+ 
+for t in range(T):
+    s = input()
+ 
+    for n in range(2, 30):
+        if s[:n] == s[n:2*n]:
+            print(f'#{t+1} {n}')
+            break
+
+# KOREAKOREAKOREAKOREAKOREAKOREA
+# 5
