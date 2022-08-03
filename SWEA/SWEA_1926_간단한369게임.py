357716d05c3366194e09279a100e53c101a761c7
김수형 <dook4966@gmail.com>
2022-08-03 17:55:11 +0900

    0803

--- SWEA/SWEA_1926_간단한369게임.py
+++ SWEA/SWEA_1926_간단한369게임.py
@@ -12,5 +12,22 @@ for n in range(1,N+1):
     else:
         print(f'{n} ', end='')
 
+
+# 새로운 풀이
+# 근데 시간 더 오래 걸림
+keyword = '369'
+N = int(input())
+
+for n in range(1,N+1):
+    n = str(n)
+    cnt = 0
+    for k in keyword:
+        if k in n:
+            cnt += n.count(k)
+    if cnt != 0:
+        print('-' * cnt + ' ', end='')
+    else:
+        print(f'{n} ', end='')
+
 # 10
 # 1 2 - 4 5 - 7 8 - 10
