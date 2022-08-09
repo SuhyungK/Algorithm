aedcfba6427691b1f667dcaa719ed86ea37e8a8e
김수형 <dook4966@gmail.com>
2022-08-09 15:10:16 +0900

    0809

--- SWEA/SWEA_4828_minmax.py
+++ SWEA/SWEA_4828_minmax.py
@@ -1,14 +1,14 @@
 T = int(input())
 for test_case in range(1, T + 1):
-    _ = input()
+    cnt = int(input())
     arr = list(map(int, input().split()))
-    max, min = arr[0], arr[0]
+    max = min = arr[0]
 
     # max
-    for M in arr:
-        max = M if max < M else max
+    for M in range(cnt):
+        max = arr[M] if max < arr[M] else max
     # min
-    for m in arr:
-        min = m if min > m else min
+    for m in range(cnt):
+        min = arr[m] if min > arr[m] else min
 
     print(f'#{test_case} {max-min}')
