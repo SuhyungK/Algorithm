c8ca466aae9b5dcef4db7bf401e6d74ff767d965
SuhyungK <dook4966@gmail.com>
2022-10-04 22:27:09 +0900

    1004

---
+++ Baekjoon/Silver/baekjoon_10816_숫자카드2.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+N = int(input())
+card = Counter(list(map(int, input().split())))
+M = int(input())
+lst = list(map(int, input().split()))
+
+for i in range(M):
+    if card.get(lst[i]):
+        lst[i] = card[lst[i]]
+    else:
+        lst[i] = 0
+
+print(*lst)
---
+++ Baekjoon/Silver/baekjoon_10866_덱.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+queue = deque()
+for _ in range(int(input())):
+    tmp = list(input().split())
+    od = tmp[0]
+
+    if od == 'push_front':
+        queue.appendleft(tmp[1])
+    elif od == 'push_back':
+        queue.append(tmp[1])
+    elif od == 'pop_front':
+        if queue: print(queue.popleft())    
+        else: print(-1)
+    elif od == 'pop_back':
+        if queue: print(queue.pop())
+        else: print(-1)
+    elif od == 'size':
+        print(len(queue))
+    elif od == 'empty':
+        if queue: print(0)
+        else: print(1)
+    elif od == 'front':
+        if queue: print(queue[0])
+        else: print(-1)
+    else:
+        if queue: print(queue[-1])
+        else: print(-1)
