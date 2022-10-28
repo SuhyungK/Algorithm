af2424f5b9d05975df3d02ac8e3a6a11962babf8
SuhyungK <dook4966@gmail.com>
2022-10-28 15:39:58 +0900

    1028 백준 16234 - 인구이동

--- Baekjoon/Gold/baekjoon_16234_인구이동.py
+++ Baekjoon/Gold/baekjoon_16234_인구이동.py
@@ -1,34 +1,39 @@
-import sys
-sys.stdin = open('test.txt')
+def bfs():
+    flag = False
+    for i in range(N):
+        for j in range(N):
+            if visit[i][j] == time -1:
+                unn = {}
+                q = [(i, j)]
+                while q:
+                    r, c = q.pop(0)
+                    for k in range(4):
+                        nr, nc = r + dr[k], c + dc[k]
+                        if -1 < nr < N and -1 < nc < N and visit[nr][nc] != time:
+                            if L <= abs(arr[nr][nc] - arr[r][c]) <= R:
+                                visit[nr][nc] = time
+                                q.append((nr, nc))
+                                unn[(nr, nc)] = arr[nr][nc]
+                if unn:
+                    flag = True
+                    l = len(unn.keys())
+                    ppl = sum(unn.values()) // l
+                    for kr, kv in list(unn.keys()):
+                        arr[kr][kv] = ppl
+    return flag
+
 
 N, L, R = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
+visit = [[-1] * N for _ in range(N)]
+
 dr = [1, -1, 0, 0]
 dc = [0, 0, 1, -1]
 
-dic = {}
-for r in range(N):
-    for c in range(N):
-        dic[(r, c)] = (r, c)
+time = 0
+while 1:
+    if not bfs():
+        break
+    time += 1
 
-Q = {}
-# while Q:
-#     Q = {}
-for k, v in dic.items():
-    r, c = k
-    if dic[(r, c)] == (r, c):
-        q = [(r, c)]
-        Q[(r, c)] = arr[r][c]
-        cnt = 1
-        while q:
-            rr, cc = q.pop(0)
-            for i in range(4):
-                nr, nc = rr + dr[i], cc + dc[i]
-                if dic.get((nr, nc)) and dic.get((nr, nc)) != (r, c) and L <= abs(arr[rr][cc] - arr[nr][nc]) <= R:
-                    dic[(nr, nc)] = (r, c)
-                    Q[(r, c)] += arr[nr][nc]
-                    cnt += 1
-            print(Q)
-        Q[(r, c)] //= cnt 
-
-    
+print(time)
