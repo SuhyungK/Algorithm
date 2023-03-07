2e17f1806091633d23f710039ae93c0eacd26355
suhyungK <dook4966@gmail.com>
2023-03-07 20:46:46 +0900

    baekjoon_17220

--- Baekjoon/Gold/baekjoon_17220.py
+++ Baekjoon/Gold/baekjoon_17220.py
@@ -1,15 +1,33 @@
-# G4 마약수사대
-import sys
-sys.stdin = open('input.txt')
-
 N, M = map(int, input().split())
-drugs = {}
+
+pos_pub, drugs = ['O'] * N, {}
 for _ in range(M):
-    b, f = map(lambda x: ord(x) - 65, input().split())
+    f, b = map(lambda x: ord(x) - 65, input().split())
+    pos_pub[b]= '.'
     if drugs.get(b):
         drugs[b].append(f)
     else:
         drugs[b] = [f]
 
-pub_num, *pub_lst = map(lambda x: ord(x) - 65, input().split())
-print(drugs)
+real_pub = input().split()
+for real in real_pub[1:]:
+    real = ord(real) - 65
+    pos_pub[real] = 'X'
+
+def dfs(s):
+    if pos_pub[s] in ('O', 'X'):
+        return pos_pub[s] == 'O'
+    
+    for e in drugs[s]:
+        if dfs(e):
+            pos_pub[s] = 'O'
+            return 1
+    pos_pub[s] = 'X'
+    return 0
+
+ans = 0
+for s in range(N):
+    if pos_pub[s] == '.':
+        ans += dfs(s)
+
+print(ans)
