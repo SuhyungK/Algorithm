0f30da18e746b13dead354765d8e4fa13c29a2e4
SuhyungK <dook4966@gmail.com>
2023-03-16 10:00:58 +0900

    230316 baekjoon 2457

--- Baekjoon/Gold/baekjoon_2457.py
+++ Baekjoon/Gold/baekjoon_2457.py
@@ -1,28 +1,39 @@
 # G3 공주님의 정원
 
-"""
-1. **꽃이 지는 날짜 기준**이 속할 수 있는 날짜 구간을 찾은 다음에 그 구간들 가운데서 가장 오랫동안 피어 있을 수 있는 구간을 찾는다
-2. 그 구간의 꽃이 지는 날짜를 다음 **꽃이 지는 날짜 기준**으로 정한다
-    
-    2-1. 이렇게 정해질 때 꽃의 개수에 카운트 + 1 을 해주면 된다
-    
-    2-2. 꽃이 지는 날짜 기준이 속할 수 있는 날짜 구간을 찾지 못한 상태로 꽃이 피는 날짜가 11월 30일을 넘어선다면 꽃의 개수는 0으로 초기화하고 종료된다
-    
-3. 1, 2의 과정을 반복한다
-4. 꽃이 지는 날짜 기준이 11월 30일을 넘어서게 된다면 종료한다
-"""
 import sys
-sys.stdin = open('input.txt')
+input = sys.stdin.readline
 
 N = int(input())
-months = [0, 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-s = sum(months[:4]) + 1
-e = sum(months) - 31
-for m in range(2, 13):
-    months[m] += months[m-1]
 
-flw = [(months[m1] + d1, months[m2] + d2) for m1, d1, m2, d2 in (map(int, input().split()) for _ in range(N))]
-flw.sort(key=lambda x: (x[0], -x[1]))
+flw = [(100 * m1 + d1, 100 * m2 + d2) for m1, d1, m2, d2 in (map(int, input().split()) for _ in range(N))]
+flw.sort()
 
-while s <= e:
-    pass
+# now : 마지막 꽃이 지는 날짜(3월 1일부터 시작)
+# ans : 개수 
+# max_end : 가장 마지막 꽃이 지는 날짜
+now, ans, max_end = 301, 0, 0
+for s, e in flw:
+    if s == e: # 그 날 피어서 그 날 지는 경우 제외
+        continue
+
+    if s <= now: # 현재 비교하고 있는 꽃의 피는 날짜 < 마지막 꽃이 지는 날짜
+        if now < e and max_end < e: # 마지막 꽃이 지는 날짜 < 현재 비교하고 있는 꽃의 지는 날짜 여야만 비교 가능
+            max_end = e
+        elif e <= now: # 마지막 꽃이 지는 날짜가 현재 비교하고 있는 꽃의 지는 날짜보다 크다면 비교할 이유 X
+            continue
+    else:
+        if max_end < s: # 가장 늦게 지는 꽃의 날짜가 다음 비교해야 할 꽃의 피는 날짜보다 작다면 연결되지 않으므로 종료
+            break
+        else:
+            """
+            만약 현재 비교하고 있는 꽃의 피는 날짜가 현재로서 가장 마지막 꽃이 지는 날짜(now)보다 크다면
+            연결은 되지만 다음 비교를 위해서는 now를 업데이트 해줘야 함
+            """
+            now, max_end = max_end, e
+            ans += 1
+
+    if e > 1130:
+        ans += 1     
+        break
+
+print(ans if max_end > 1130 else 0)
