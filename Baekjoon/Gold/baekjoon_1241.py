6fa5c46f7b86485bf952a984f9898cd7f7c71835
SuhyungK <dook4966@gmail.com>
2023-03-17 17:56:16 +0900

    baekjoon_1241

--- Baekjoon/Gold/baekjoon_1241.py
+++ Baekjoon/Gold/baekjoon_1241.py
@@ -6,11 +6,12 @@ for l in lst:
     if matrix.get(l): matrix[l] += 1
     else: matrix[l] = 1
 
-ans = [0 for _ in range(N)]
+ans = [-1 for _ in range(N)]
 for i in range(N):
-    for k in range(1, (l:=lst[i])**0.5 + 1):
+    l = lst[i]
+    for k in range(1, int(l**0.5) + 1):
         if not l % k:
-            if matrix.get(k): ans[l] += matrix[k]
-            if k * k != l and matrix.get(l//k): ans[l] += matrix[l//k]
+            if matrix.get(k): ans[i] += matrix[k]
+            if k * k != l and matrix.get(l//k): ans[i] += matrix[l//k]
 
 print(ans)
