7437dfee6bf239017529c90d5b06980bcb5349aa
suhyungK <dook4966@gmail.com>
2023-04-02 16:07:57 +0900

    백준 1049

---
+++ Baekjoon/Silver/baekjoon_1049.py
@@ -0,0 +1,18 @@
+import math 
+
+N, M = map(int, input().split())
+lst = [list(map(int, input().split())) for _ in range(M)]
+lst = list(map(list, zip(*lst)))
+
+a, b = min(lst[0]), min(lst[1])
+
+res = 0
+if a // b >= 6:
+    res = math.ceil(N/6) * a
+else:
+    if (N % 6) > a // b:
+        res = math.ceil(N/6) * a
+    else:
+        res = N // 6 * a + (N % 6) * b
+
+print(res)
