16b2be423ee20c5ef527480dc2e011fecb478b49
SuhyungK <dook4966@gmail.com>
2023-04-26 08:44:23 +0900

    백준 2491

--- Baekjoon/Silver/baekjoon_2491.py
+++ Baekjoon/Silver/baekjoon_2491.py
@@ -1,2 +1,13 @@
+# 수열
+
 N = int(input())
 arr = list(map(int, input().split()))
+dp_i, dp_d = [1] * N, [1] * N
+
+for i in range(1, N):
+    if arr[i-1] <= arr[i]:
+       dp_i[i] += dp_i[i-1]
+    if arr[i-1] >= arr[i]:
+        dp_d[i] += dp_d[i-1]
+ 
+print(max(max(dp_i), max(dp_d)))
