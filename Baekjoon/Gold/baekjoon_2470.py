6c713ab39dc1e22b159b2cbf33ef01e07892d746
suhyungkim <dook4966@gmail.com>
2023-05-14 17:06:08 +0900

    백준 2470

--- Baekjoon/Gold/baekjoon_2470.py
+++ Baekjoon/Gold/baekjoon_2470.py
@@ -1,36 +1,24 @@
 # 두 용액
 
-N = int(input())
-properties = list(map(int, input().split()))
-properties.sort()
+def sol():
+    N = int(input())
+    solution = list(map(int, input().split()))
 
-for i in range(N):
-    now = properties[i]
-    left, right = i+1, N - 1
-    min_sum = float('inf')
-    while left <= right:
-        m = (left + right) // 2
-        if abs(now - properties[m]) >= min_sum:
-            left = m + 1
-        else:
-            min_sum = abs(now - properties[m])
-            right = m - 1
-print(min_sum)
-
-arr = list(map(int, input().split()))
-arr.sort()
-
-l, r = 0, N-1
-cur_res, min_dif = (), float('inf')
-while l < r:
-    cur_sum = arr[l] + arr[r]
-    if abs(cur_sum) < min_dif:
-        min_dif = abs(cur_sum)
-        cur_res = (arr[l], arr[r])
-    
-    if cur_sum > 0:
-        r -= 1
-    else:
-        l += 1
+    solution.sort()
+    l, r = 0, N-1
+    min_diff, min_sols = 1e13, []
+    while l < r:
+        tmp = solution[l] + solution[r]
+        if abs(tmp) < min_diff:
+            min_diff = abs(tmp)
+            min_sols = [l, r]
         
-print(*cur_res)
+        if tmp >= 0:
+            r -= 1
+        else:
+            l += 1
+
+    print(solution[min_sols[0]], solution[min_sols[1]])
+
+if __name__ == '__main__':
+    sol()
