db0ace056775f2181f45a2ce085393f97f6efb4a
SuhyungK <dook4966@gmail.com>
2023-05-24 17:48:59 +0900

    백준 14890

--- Baekjoon/Silver/baekjoon_14890.py
+++ Baekjoon/Silver/baekjoon_14890.py
@@ -1,7 +1,13 @@
 # 경사로
 
-def fore():
-    pass
+def fore(row, s, n): # 행, 시작점 값
+    for t in range(1, L+1):
+        if s+t>N: return N
+
+        if row[s+t] == n and not ramp[s+t]:
+            pass
+    return True
+
 
 def back():
     pass
@@ -12,6 +18,7 @@ MAP = [list(map(int, input().split())) for _ in range(N)]
 ans = 0
 for row in MAP:
     t, next = row[0], 1
+    ramp = [False]*N
     while next < N:
         if t == row[next]:
             continue
@@ -23,5 +30,8 @@ for row in MAP:
             fore()
         elif gap == 1:
             back()
+        
+        if next >= N:
+            break
     else:
         ans += 1
