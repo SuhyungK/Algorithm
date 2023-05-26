a085c59dcb9919e5c017ac061f0a61ba6c162497
SuhyungK <dook4966@gmail.com>
2023-05-26 17:46:06 +0900

    백준 19236

--- Baekjoon/Gold/baekjoon_19236.py
+++ Baekjoon/Gold/baekjoon_19236.py
@@ -1,28 +1,48 @@
-# 청소년 상어
-
-def fist_move():
-    for n in range(1, 17):
-        x, y, dir = fish_lst[n]
-
-        for d in range(1, 9):
-            nx, ny = DX[dir+d], DY[dir+d]
-            if nx<0 or ny<0 or nx>=4 or ny>=4:
+def move():
+    for i in range(1, 17):
+        if not loc.get(i):
+            continue
+        x, y = loc[i]
+        n, d = score_dir[(x, y)]
+        for i in range(8):
+            nx, ny = x+dx[(d+i)%8], y+dy[(d+i)%8]
+            if nx<0 or ny<0 or nx>3 or ny>3 or score_dir[(nx, ny)][0]<0:
                 continue
+            score_dir[(nx, ny)], score_dir[(x, y)] = score_dir[(x, y)], score_dir[(nx, ny)]
+            break
 
-        
+def revert():
+    for i in range(16, 0, -1):
+        if not loc.get(i):
+            continue
+        x, y = loc[i]
+        n, d = score_dir[(x, y)]
+        d += 4
+        for i in range(8):
+            nx, ny = x+dx[(d+i)%8], y+dy[(d+i)%8]
+            if nx<0 or ny<0 or nx>3 or ny>3 or score_dir[(nx, ny)][0]<0:
+                continue
+            score_dir[(nx, ny)], score_dir[(x, y)] = score_dir[(x, y)], score_dir[(nx, ny)]
+            break
 
-def shark_move():
+def shark_move(x, y, s): # 상어 이동, (x, y) 상어의 위치
     pass
 
-DX = (-1, -1, 0, 1, 1, 1, 0, -1)
-DY = (0, -1, -1, -1, 0, 1, 1, 1) 
-fish_lst = {}
-fish_arr = {}
-for i in range(4):
-    row = tuple(map(int, input().split()))
-    for j in range(0, 8, 2):
-        fish_lst[row[j]] = (i, j)
-        fish_arr[(i, j)] = row[j+1]
 
-print(fish_lst)
-print(fish_arr)
+dx = (-1, -1, 0, 1, 1, 1, 0, -1)
+dy = (0, -1, -1, -1, 0, 1, 1, 1)
+loc = {}
+score_dir = {}
+for i in range(4):
+    row = list(map(int, input().split()))
+    for j in range(4):
+        n, d = row[2*j:2*j+2]
+        loc[n] = [i, j]
+        score_dir[(i, j)] = [n, d-1]
+# print(loc)
+# print(score_dir)
+
+move()
+print(score_dir)
+revert()
+print(score_dir)
