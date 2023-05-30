cc28f69adda31f0788754f1868337e9c850e6199
SuhyungK <dook4966@gmail.com>
2023-05-30 17:42:35 +0900

    백준 14888

--- Baekjoon/Silver/baekjoon_14888.py
+++ Baekjoon/Silver/baekjoon_14888.py
@@ -1,36 +1,28 @@
-def calc(k, res):
+# 연산자 끼워넣기
+
+def calc(res, k, p, m, t, d):
     global max_res, min_res
     if k == N:
         max_res = max(max_res, res)
         min_res = min(min_res, res)
         return 
-    
-    prev = -1
-    for i in range(N-1):
-        if not check[i] and prev != op[i]:
-            check[i] = True
-            calc(k+1, arith[op[i]](res, nums[k]))
-            prev = op[i]
-            check[i] = False
-
+    if p:
+        calc(res+nums[k], k+1, p-1, m, t, d)
+    if m:
+        calc(res-nums[k], k+1, p, m-1, t, d)
+    if t:
+        calc(res*nums[k], k+1, p, m, t-1, d)
+    if d:
+        calc(int(res/nums[k]), k+1, p, m, t, d-1)
+      
 N = int(input())
 nums = list(map(int, input().split()))
 ops = list(map(int, input().split()))
-check = [False]*N
 
-op = []
-for i, o in enumerate(ops):
-    op.extend([i]*o)
-op.sort()
-
-arith = {
-    0: lambda x, y : int(x) + int(y),
-    1: lambda x, y : int(x) - int(y),
-    2: lambda x, y : int(x) * int(y),
-    3: lambda x, y : -(abs(int(x)) // int(y)) if int(x) < 0 else int(x) // int(y)
-}
-
-max_res, min_res = -float('inf'), float('inf')
-calc(1, nums[0])
+max_res, min_res = -1e13, 1e13
+calc(nums[0], 1, *ops)
 print(max_res)
 print(min_res)
+
+
+print(int(-4/3))
