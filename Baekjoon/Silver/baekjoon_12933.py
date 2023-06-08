0b49fdc287ef8da3d107b83b92bd8b4097506f85
suhyungk <dook4966@gmail.com>
2023-06-08 23:56:44 +0900

    백준 12933

---
+++ Baekjoon/Silver/baekjoon_12933.py
@@ -0,0 +1,23 @@
+# 오리
+
+sound = input()
+quack = [0, 0, 0, 0, 0]
+quack_index = {
+    'u': (0, 1), 'a': (1, 2), 'c': (2, 3), 'k': (3, 4)
+}
+def sol():
+    for s in sound:
+        if s == 'q':
+            if quack[-1]>0: quack[-1] -= 1
+            quack[0] += 1
+        else:
+            f, b = quack_index[s]
+            if quack[f]>0: 
+                quack[f] -= 1
+                quack[b] += 1
+
+    if quack[-1] > 0:
+        return quack[-1]
+    return -1
+
+print(sol())
