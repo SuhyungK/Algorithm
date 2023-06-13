7ccf6a7cd0b65556781500202276275e324a12e6
suhyungk <dook4966@gmail.com>
2023-06-13 21:24:44 +0900

    SWEA 1952

--- SWEA/1952.py
+++ SWEA/1952.py
@@ -16,9 +16,15 @@ def calc_fee(n, fee):
 
 for tc in range(1, int(input())+1):
     day, one, triple, year = map(int, input().split())
-    plans = list(map(int, input().split()))
+    plans = [0]+list(map(int, input().split()))
     ans = year
+    dp = [0]*13
+
+    dp[1] = min(day*plans[1], one)
+    dp[2] = dp[1]+min(day*plans[2], one)
+    for i in range(3, 13):
+        dp[i] = min(dp[i-3]+triple, dp[i-1]+min(one, day*plans[i]))
 
     calc_fee(0, 0)
-
-    print(f'#{tc}', ans)
+    print(f'#{tc}', min(dp[-1], year))
+    print(dp)
