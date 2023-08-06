da6c2bb7d480346a5fce336462e2b01a3b24b00a
suhyungk <dook4966@gmail.com>
2023-08-06 16:58:49 +0900

    백준 14606

--- Baekjoon/Silver/baekjoon_14606.py
+++ Baekjoon/Silver/baekjoon_14606.py
@@ -1,19 +1,22 @@
+# 피자(Small)
+
 N = int(input())
 
 def solution():
-    if N == 3 or N == 5:
+    if N == 1:
+        return 0
+    
+    if N == 2:
         return 1
     
-    if N == 4:
-        return -1
+    dp = [0] * (N+1)
+    dp[2] = 1
     
-    dp = [1e9]*(N+1)
-    dp[3] = dp[5] = 1
-    for i in range(6, N+1):
-        dp[i] = min(dp[i-3], dp[i-5])+1
-       
-    if dp [-1] == 1e9:
-        return -1
+    for i in range(3, N+1):
+        a, b = i//2, i-i//2
+        dp[i] = dp[a] + dp[b] + a*b
+    
+    # print(dp)
     return dp[-1]
 
 print(solution())
