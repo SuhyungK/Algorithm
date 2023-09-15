b2d522e0f1373ebf223d91bea54951283393dd94
suhyungk <dook4966@gmail.com>
2023-09-15 13:15:32 +0900

    fix: swea 1952

--- SWEA/1952.py
+++ SWEA/1952.py
@@ -1,30 +1,28 @@
 # 수영장
 
-import sys
-sys.stdin = open('input.txt')
-
 def calc_fee(n, fee):
     global ans
-    # print(n, fee)
     if n >= 12:
+        if ans > fee:
+            print(fee)
         ans = min(ans, fee)
         return
 
     calc_fee(n+1, fee+day*plans[n])
     calc_fee(n+1, fee+one)
-    calc_fee(n+3, fee+triple)    
+    calc_fee(n+3, fee+triple)
 
 for tc in range(1, int(input())+1):
     day, one, triple, year = map(int, input().split())
     plans = [0]+list(map(int, input().split()))
     ans = year
-    dp = [0]*13
+    # dp = [0]*13
 
-    dp[1] = min(day*plans[1], one)
-    dp[2] = dp[1]+min(day*plans[2], one)
-    for i in range(3, 13):
-        dp[i] = min(dp[i-3]+triple, dp[i-1]+min(one, day*plans[i]))
+    # dp[1] = min(day*plans[1], one)
+    # dp[2] = dp[1]+min(day*plans[2], one)
+    # for i in range(3, 13):
+    #     dp[i] = min(dp[i-3]+triple, dp[i-1]+min(one, day*plans[i]))
 
     calc_fee(0, 0)
-    print(f'#{tc}', min(dp[-1], year))
-    print(dp)
+    # print(f'#{tc}', min(dp[-1], year))
+    # print(dp)
