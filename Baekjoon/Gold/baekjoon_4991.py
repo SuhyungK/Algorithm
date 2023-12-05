3e2eead8cbea411bae65e13e1ed181c0b772e9ee
SuhyungK <dook4966@gmail.com>
2023-12-05 18:42:38 +0900

    BOJ 4991

--- Baekjoon/Gold/baekjoon_4991.py
+++ Baekjoon/Gold/baekjoon_4991.py
@@ -1,7 +1,6 @@
 # 로봇 청소기 
 
 import sys
-sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 def find_start(arr):
@@ -26,11 +25,7 @@ def bfs(arr, start, dirty):
     target_bitmask = (1 << len(dirty)) - 1
     while queue:
         x, y, cnt, bitmask = queue.pop(0)
-        print(x, y, cnt, bin(bitmask)[2:].zfill(2))
         if bitmask == target_bitmask:
-            # print(x, y)
-            # for row in visited:
-            #     print(row)
             return cnt
         
         for nx, ny in (x-1, y), (x+1, y), (x, y-1), (x, y+1):
@@ -56,15 +51,3 @@ while True:
     ROOM = [list(input()) for _ in range(h)]
     dirty = find_dirty(ROOM)
     print(bfs(ROOM, find_start(ROOM), dirty))
-
-
-# check_result = []
-# while True:
-#     Input = int(input())
-#     if not Input:
-#         break
-#     check_result.append(Input)
-
-# for i in range(len(check_result)):
-#     if result[i] != check_result[i]:
-#         print(i, end=" ")
