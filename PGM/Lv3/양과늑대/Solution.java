83f6a2f9b4954ec4c6d321f50424e0a1bcff520e
SuhyungK <dook4966@gmail.com>
2023-12-09 14:11:59 +0900

    PGM 양과 늑대

--- PGM/Lv3/양과늑대/Solution.java
+++ PGM/Lv3/양과늑대/Solution.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class Solution {
 
+    // 2진 트리 모양 초원 정보
     List<List<Integer>> grassLand = new ArrayList<>();
     int maxSheep;
     int[] _info;
@@ -24,10 +25,14 @@ public class Solution {
     }
 
     public void dfs(int now, int[] state, List<Integer> queue) {
+        // state : [양의 개수, 늑대의 개수]
         state[_info[now]] += 1;
         maxSheep = Math.max(maxSheep, state[0]);
-        if (state[0] <= state[1]) return;
 
+        // 양의 개수 = 늑대의 개수면 늑대가 양을 다 잡아먹기 때문에 return 
+        if (state[0] <= state[1]) return;
+        
+        // 현재 노드의 자식 노드들을 큐에 전부 추가
         queue.addAll(grassLand.get(now));
         for (int i = 0; i < queue.size(); i++) {
             int next = queue.get(i);
@@ -35,6 +40,7 @@ public class Solution {
             dfs(next, Arrays.copyOf(state, 2), queue);
             queue.add(i, next);
         }
+        // 
         queue.removeAll(grassLand.get(now));
     }
 }
