5594695e50d5caeda2861ad881e0d905e938e5e2
SuhyungK <dook4966@gmail.com>
2023-12-14 20:01:03 +0900

    BOJ 2003

---
+++ Baekjoon/Silver/baekjoon_2003.java
@@ -0,0 +1,38 @@
+package Baekjoon.Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class baekjoon_2003 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[N];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int p1 = 0; int p2 = 0;
+        int sum = 0;
+        int answer = 0;
+
+        while (!(sum < M && p2 >= N)) {
+            if (sum == M) answer++;
+            
+            if (sum >= M) {
+                sum -= arr[p1];
+                p1++;
+            } else {
+                sum += arr[p2];
+                p2++;
+            }
+        }
+        System.out.println(answer);
+    }
+}
