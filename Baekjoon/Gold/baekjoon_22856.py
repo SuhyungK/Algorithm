1e848f66b89170c31ca2ec3189b79945142329d2
SuhyungK <dook4966@gmail.com>
2023-12-18 15:28:09 +0900

    BOJ 22856

--- Baekjoon/Gold/baekjoon_22856.py
+++ Baekjoon/Gold/baekjoon_22856.py
@@ -5,14 +5,12 @@ sys.setrecursionlimit(1000000)
 
 def traversal(i, cnt):
     if tree[i][0] != -1:
-        print(i, "번째 노드1", cnt)
         cnt = traversal(tree[i][0], cnt+1) + any(visited)
     visited[i] = False
     if tree[i][1] != -1:
-        print(i, "번째 노드2", cnt)
         cnt = traversal(tree[i][1], cnt+1) + any(visited)
-    print(i, "번째 노드3", cnt)
     return cnt
+
 N = int(input())
 tree = [[] for _ in range(N+1)]
 for _ in range(N):
