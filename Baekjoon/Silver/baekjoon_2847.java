9c733189b63f3b7cd96c51a935e675f319b479cf
SuhyungK <dook4966@gmail.com>
2023-12-19 20:46:37 +0900

    BOJ 2847

---
+++ Baekjoon/Silver/baekjoon_2847.java
@@ -0,0 +1,23 @@
+package Baekjoon.Silver;
+
+import java.util.Scanner;
+
+public class baekjoon_2847 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] levels = new int[N];
+        for (int i = 0; i < levels.length; i++) {
+            levels[i] = sc.nextInt();
+        }
+
+        int answer = 0;
+        for (int i = levels.length-1; i > 0; i--) {
+            if (levels[i] > levels[i-1]) continue;
+            answer += levels[i-1] - levels[i] + 1;
+            levels[i-1] = levels[i] - 1;
+        }
+
+        System.out.println(answer);
+    }
+}
