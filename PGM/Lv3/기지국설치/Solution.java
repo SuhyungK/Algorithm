89dbcebe42b738d27b5cd1b5ead306be37b6fc24
suhyungk <dook4966@gmail.com>
2024-07-25 16:05:52 +0900

    PGM 기지국 설치

---
+++ PGM/Lv3/기지국설치/Solution.java
@@ -0,0 +1,22 @@
+package PGM.Lv3.기지국설치;
+
+import java.util.*;
+
+public class Solution {
+    public int solution(int[][] routes) {
+        Arrays.sort(routes, (r1, r2) -> {
+            return r1[1] - r2[1];
+        });
+
+        int endTime = -30001;
+        int answer = 0;
+        for (int[] route : routes) {
+            if (endTime < route[0]) {
+                endTime = route[1];
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
